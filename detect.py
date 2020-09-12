ac9d3e64835fcb1e097abe37733f128144f553a3
Jason Hu <35642868+jasonhu09@users.noreply.github.com>
2020-09-12 17:09:52 -0400

    Updated Task 3
    Command to python detect.py "filename"

--- detect.py
+++ detect.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import typing as T
 import matplotlib.pyplot as plt
 import numpy as np
+import statistics
 
 def load_data(file: Path) -> T.Dict[str, pandas.DataFrame]:
     #create 3 list for each different room
@@ -35,20 +36,20 @@ def load_data(file: Path) -> T.Dict[str, pandas.DataFrame]:
     #calculating standard deviation and mean of lab, then printing
     standev_lab = np.std(temperature_lab)
     mean_lab = np.mean(temperature_lab)
-    print ("Standard Deviation of Lab :", standev_lab)
-    print ("Average of Lab:", mean_lab, "\n")
+    print ("Standard Deviation of Lab (with bad data): ", standev_lab)
+    print ("Average of Lab (with bad data): ", mean_lab, "\n")
 
     #calculating standard deviation and mean of class, then printing
     standev_class = np.std(temperature_class)
     mean_class = np.mean(temperature_class)
-    print ("Standard Deviation of Class :", standev_class)
-    print ("Average of Class:", mean_class, "\n")
+    print ("Standard Deviation of Class (with bad data) :", standev_class)
+    print ("Average of Class (with bad data):", mean_class, "\n")
 
     #calculating standard deviation and mean of office, then printing
     standev_office = np.std(temperature_office)
     mean_office = np.mean(temperature_office)
-    print ("Standard Deviation of Office :", standev_office)
-    print ("Average of Office:", mean_office, "\n")
+    print ("Standard Deviation of Office (with bad data) :", standev_office)
+    print ("Average of Office (with bad data):", mean_office, "\n")
 
     #finding the length of each temperature list as data is different for each person
     len_lab = len(temperature_lab);
@@ -57,9 +58,19 @@ def load_data(file: Path) -> T.Dict[str, pandas.DataFrame]:
 
     #run a for loop on each temperature list to find anomalies within each one
     #our condition is that the temperature should stay within the mean +/- 2 standard deviations
+    bad_lab_count = 0
+    bad_data = []
+    #lab was the chosen room for Task 2
+
     for x in range (1,len_lab):
         if (temperature_lab[x] > (((2 * (standev_lab)) + mean_lab) or temperature_lab[x] < (mean_lab - (2 * (standev_lab))))):
+            bad_lab_count += 1
+            bad_data.append(x);
             print("The", x, "frame of the lab is an anomoly.")
+
+    for x in range (1, len(bad_data)):
+        y = bad_data[x]
+        temperature_lab.pop(y)
 
     for x in range (1,len_office):
         if (temperature_office[x] > (((2 * (standev_office)) + mean_office) or temperature_office[x] < (mean_office - (2 * (standev_office))))):
@@ -68,6 +79,17 @@ def load_data(file: Path) -> T.Dict[str, pandas.DataFrame]:
     for x in range (1,len_class):
         if (temperature_class[x] > (((2 * (standev_class)) + mean_class) or temperature_class[x] < (mean_class - (2 * (standev_class))))):
             print("The", x, "frame of the class is an anomoly.")
+
+    percent_baddata = bad_lab_count/len_lab
+    standev_lab = np.std(temperature_lab)
+    mean_lab = np.mean(temperature_lab)
+
+    print ("\nFor Lab 1, there was", bad_lab_count, "percent of bad data.")
+    print ("Standard Deviation of lab (with good data):", standev_lab)
+    print ("Average of lab (with good data):", mean_lab)
+    print ("The median of the data set of lab is %s" %(statistics.median(temperature_lab)))
+    print ("The variance of the data set of lab is %s" %(statistics.variance(temperature_lab)))
+
 
 
 if __name__ == "__main__":
